ee767bdcfbe468abf4af40b8f7c03876379a84f0
hosurVijay <hosurvijayendra@gmail.com>
2025-08-11 18:51:30 +0530

    User controller updated with OTP Feature and small bugs fixed

--- backend/src/controller/user.contorller.js
+++ backend/src/controller/user.contorller.js
@@ -2,7 +2,8 @@ import { asyncHandler } from "../Utils/asyncHandler.js";
 import { User } from "../Modles/user.model.js";
 import { ApiError } from "../Utils/ApiError.js";
 import { ApiResponse } from "../Utils/ApiResponse.js";
-import { use } from "react";
+import { sendToUserEMailId } from "../Utils/otpVerity.js";
+
 const generateAccessRefreshToken = async (userID) => {
   try {
     const user = await User.findById(userID);
@@ -22,7 +23,7 @@ const signUpUser = asyncHandler(async (req, res) => {
   const { username, fullname, email, password, confirmPassword } = req.body;
   if (
     [username, fullname, email, password, confirmPassword].some(
-      (field) => field?.trim === ""
+      (field) => field?.trim() === ""
     )
   ) {
     throw new ApiError(400, "all fields are required");
@@ -33,7 +34,7 @@ const signUpUser = asyncHandler(async (req, res) => {
   }
 
   const existUser = await User.findOne({
-    $or: [{ username }, { email }],
+    $or: [{ username: username.toLowerCase() }, { email }],
   });
   if (existUser) {
     throw new ApiError(400, "User already exist with same username and email");
@@ -44,30 +45,198 @@ const signUpUser = asyncHandler(async (req, res) => {
     fullname,
     email,
     password,
+    isVerified: false,
   });
 
-  const { refreshToken, accessToken } = await User.findById(
-    user._id
-  ).generateAccessRefreshToken();
+  const userOtp = user.generateOtp();
+  await user.save();
 
-  const createdUser = await User.findById(user._id).select(
-    "-password -refreshToken"
+  await sendToUserEMailId(
+    user.email,
+    "your 6-digit OTP code for Signup",
+    `Your OTP is : ${userOtp}. "This will expiry in 5 minutes"`
   );
+
+  const createdUser = await User.findById(user._id);
+
   if (!createdUser) {
     throw new ApiError(400, "Failed to create Account");
   }
 
+  res
+    .status(200)
+    .json(new ApiResponse(200, "Account created Successfully", { email }));
+});
+
+const verifyUserOtp = asyncHandler(async (req, res) => {
+  const { email, userOtp } = req.body;
+
+  if (!email || !userOtp) {
+    throw new ApiError(500, "Email and Otp required.");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new ApiError(200, "Falied to fetch user.");
+  }
+
+  if (user.otp !== userOtp) {
+    throw new ApiError(500, "Otp did't match.");
+  }
+
+  if (Date.now() > user.otpExpiry) {
+    throw new ApiError(
+      500,
+      "Invalid OTP or OTP expired. \n Please request a new OTP."
+    );
+  }
+
+  const { refreshToken, accessToken } = user.generateAccessRefreshToken();
+  user.isVerified = true;
+  user.otp = null;
+  user.otpExpiry = null;
+  await user.save();
+
+  const safeUserInfo = await User.findById(user?._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!safeUserInfo) {
+    throw new ApiError(500, "No user found.");
+  }
+
   res.cookie("accessToken", accessToken, {
     httpOnly: true,
-    sameSite: "LAX",
     secure: false,
+    sameSite: "LAX",
     maxAge: 10 * 24 * 60 * 60 * 1000,
   });
   res
     .status(200)
-    .json(new ApiResponse(200, "Account created Successfully", createdUser));
+    .json(new ApiResponse(200, "Account verified successful.", safeUserInfo));
 });
 
+const resendOtp = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+  if (!email) {
+    throw new ApiError(400, "Email is required or missing");
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new ApiError(400, "No User found.");
+  }
+
+  if (user.isVerified) {
+    throw new ApiError(500, "Account is Already verified.");
+  }
+
+  const nowDate = Date.now();
+
+  if (!user.otpRequest) user.otpRequest = [];
+
+  user.otpRequest = user.otpRequest.filter(
+    (timestamp) => nowDate - timestamp < 30 * 60 * 1000
+  );
+
+  if (user.otpRequest.length >= 3) {
+    throw new ApiError(
+      400,
+      "Too many Otp request. Try again after 30 minutes."
+    );
+  }
+
+  const newOtp = user.generateOtp();
+  user.otpRequest.push(nowDate);
+  await user.save();
+
+  await user.sendToUserEMailId(
+    user.email,
+    "Your new OTP for sign Up",
+    `"Your new OTP is" ${newOtp}. this will expriy in 5 minutes.`
+  );
+
+  res.status(200).json(new ApiResponse(200, "OTP sent successful", { email }));
+});
+
+const forgotPassword = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+  if (!email) {
+    throw new ApiError(500, "Email is required.");
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new ApiError(400, "No user found.");
+  }
+
+  const resetOtp = user.generateOtp();
+  user.otpExpiry = Date.now() + 5 * 60 * 1000;
+  await user.save();
+
+  await sendToUserEMailId(
+    user.email,
+    "Your OTP to rest the Password",
+    `"Your password to reset the Password" ${resetOtp}`
+  );
+
+  res.status(200).json(
+    new ApiResponse(200, "Otp to Reset the password sent successful", {
+      email,
+    })
+  );
+});
+
+const verifyResetOtp = asyncHandler(async (req, res) => {
+  const { email, otp } = req.body;
+
+  if (!(email || otp)) {
+    throw new ApiError(400, "Email and otp is required.");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new ApiError(400, "No user found");
+  }
+
+  if (Date.now() > user.otpExpiry) {
+    throw new ApiError("Invalid OTP or otp expired.");
+  }
+
+  if (user.otp !== otp) {
+    throw new ApiError(400, "Otp didn't match. Try again");
+  }
+
+  res.status(200).json(new ApiResponse(200, "OTP verified Successfully", null));
+});
+
+const resetPassword = asyncHandler(async (req, res) => {
+  const { email, newPassword, confrimNewPassword } = req.body;
+
+  if (!email || !newPassword || !confrimNewPassword) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  if (newPassword !== confrimNewPassword) {
+    throw new ApiError(500, "Both Password didn't match. Try again");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new ApiError(400, "NO user found");
+  }
+  user.password = newPassword;
+  user.otp = null;
+  user.otpExpiry = null;
+  await user.save();
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, "Password changed successfully", null));
+});
 const loginUser = asyncHandler(async (req, res) => {
   const { username, password, email } = req.body;
 
